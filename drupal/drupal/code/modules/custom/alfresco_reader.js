2c1b64d5797a72615bd420cc865f4e5ea002c00a
Paul Hedberg <paul.hedberg@nateko.lu.se>
2015-09-09 08:48:19 +0200

    Some name corrections.

--- drupal/drupal/code/modules/custom/alfresco_reader.js
+++ drupal/drupal/code/modules/custom/alfresco_reader.js
@@ -1,18 +1,6 @@
-jQuery(document).ready(function($) {
-	var $ = jQuery.noConflict();
-	console.log('JQuery version ' + $.fn.jquery);	
-});
-
 (function ($) {
 	
 }(jQuery));
-
-
-(function () {
-
-}());
-
-
 
 
 
