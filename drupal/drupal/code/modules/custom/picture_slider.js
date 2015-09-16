86d0f772db807e35bd4760a7ec697af7d0607053
Paul Hedberg <paul.hedberg@nateko.lu.se>
2015-09-16 15:58:44 +0200

    Some adjustments to the Picture Slider demo.

--- drupal/drupal/code/modules/custom/picture_slider.js
+++ drupal/drupal/code/modules/custom/picture_slider.js
@@ -3,8 +3,8 @@
 		attach: function (context, settings) {	
 			if (settings.picture_slider.color) {   
 		        $('.block_titel').css({'color': settings.picture_slider.color});
-		        $('.glyphicon-chevron-down').css('color', settings.picture_slider.color);
-		        $('.glyphicon-chevron-up').css('color', settings.picture_slider.color);	             	   
+		        $('.glyphicon-chevron-left').css('color', settings.picture_slider.color);
+		        $('.glyphicon-chevron-right').css('color', settings.picture_slider.color);	             	   
   			}	    
 		}	
 	};		
