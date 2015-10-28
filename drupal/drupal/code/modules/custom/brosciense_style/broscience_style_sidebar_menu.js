9a7dd3ff2082060ef57182350bb917fb7bb4dd65
Paul Hedberg <paul.hedberg@nateko.lu.se>
2015-10-28 10:40:52 +0100

    Add nice arrow to active element in submenu for Drupal Broscience style.

--- drupal/drupal/code/modules/custom/brosciense_style/broscience_style_sidebar_menu.js
+++ drupal/drupal/code/modules/custom/brosciense_style/broscience_style_sidebar_menu.js
@@ -2,18 +2,22 @@
     Drupal.behaviors.broscience_style = {
         attach: function (context, settings) {
 
-            style_bs_submenu(settings.broscience_style.elementColor, settings.broscience_style.modulePath);
+            style_bs_submenu(settings.broscience_style.elementColor);
             place_bs_submenu();
+            set_bs_active(settings.broscience_style.elementColor);
 
             $(window).resize(function() {
-                style_bs_submenu();
+                style_bs_submenu(settings.broscience_style.elementColor);
                 place_bs_submenu();
+                set_bs_active(settings.broscience_style.elementColor);
             });
         }
     };
 }(jQuery));
 
+
 var min = 800;
+
 
 function place_bs_submenu() {
     if (jQuery(window).width() > min) {
@@ -28,9 +32,8 @@ function place_bs_submenu() {
 }
 
 
-function style_bs_submenu(elementColor, modulePath) {
+function style_bs_submenu(elementColor) {
     if (jQuery(window).width() > min) {
-
         jQuery('#block-menu-block-1 .content ul li a').css({'padding': '0'});
         jQuery('#block-menu-block-1 .content ul li.leaf a').css({'padding': '0'});
         jQuery('#block-menu-block-1 .content ul.menu li a').css({'padding': '0'});
@@ -47,15 +50,17 @@ function style_bs_submenu(elementColor, modulePath) {
         jQuery('#block-menu-block-1 li.expanded a.active-trail').css({'color': '#414042'});
         jQuery('#block-menu-block-1 a.active').css({'color': '#414042'});
 
-        jQuery('#block-menu-block-1 a.active').append('<span style="margin-left: 0.4rem; color: #414042">&#8680;</span>');
-
         jQuery('#block-menu-block-1 a').mouseover(function() {
             jQuery(this).css('background-color', 'rgba(0, 0, 0, 0.1)');
         });
         jQuery('#block-menu-block-1 a').mouseout(function() {
             jQuery(this).css('background-color', elementColor);
         });
-
-
     }
+}
+
+
+function set_bs_active(elementColor) {
+    var w = jQuery('#block-menu-block-1').outerWidth() - 1;
+    jQuery('#block-menu-block-1 a.active').append('<span style="position: absolute; left: ' + w + 'px; width: 0; height: 0; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-left: 20px solid ' + elementColor + ';">&nbsp;&nbsp;&nbsp;&nbsp;</span>');
 }
