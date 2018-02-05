82e64968c22112bb3e0de8446a109f0b2ec65c9a
Jonathan Thiry <github@jonathanthiry.com>
2018-02-05 14:43:07 +0100

    Use a variable to set a different drupal database name for Nordspec

--- devops/roles/icos.drupal/templates/backup.sh
+++ devops/roles/icos.drupal/templates/backup.sh
@@ -12,7 +12,7 @@ tar -czf $backup_folder/files/files_backup.tar.gz /disk/data/drupal/{{ project_n
 tar -czf $backup_folder/files/modules_backup.tar.gz /disk/data/drupal/{{ project_name }}/drupal/modules/
 tar -czf $backup_folder/files/themes_backup.tar.gz /disk/data/drupal/{{ project_name }}/drupal/themes/
 
-docker exec {{ project_name }}_mariadb_1 sh -c 'mysqldump -u root --password=$MYSQL_ROOT_PASSWORD icos' > $backup_folder/db/{{ project_name }}_db_dump.sql && echo "MySQL dump finished successfully"
+docker exec {{ project_name }}_mariadb_1 sh -c 'mysqldump -u root --password=$MYSQL_ROOT_PASSWORD {{ database_name | default("icos") }}' > $backup_folder/db/{{ project_name }}_db_dump.sql && echo "MySQL dump finished successfully"
 
 # Keep only the last 30 backups
 find $backup_path -maxdepth 1 -type d -name '{{ project_name }}DrupalBackup-*' | sort -r | tail -n +31 | xargs -r rm -r
