82a326363ad7d5cf711e632d1faa08ba61e84357
André Bjärby <andre.bjarby@nateko.lu.se>
2018-11-09 09:54:34 +0100

    icos.rdflog - make the sql status files work properly on replicas as well
    We can't use 'create function' on replicas since replicas are readonly. We also
    want to be able to upgrade the sql status report code while the database is
    running (as opposed to during initialization).

--- devops/roles/icos.rdflog/templates/status.sql
+++ devops/roles/icos.rdflog/templates/status.sql
@@ -1,8 +1,11 @@
+-- This is the status report used the rdflog's 'ctl' tool.
+
 \pset footer
 \pset title
 
 \connect {{ rdflog_db_name }};
 
+-- This part shows replication status on the master side.
 select
   case
     when s is null then 'No slots are currently replicating'
@@ -12,5 +15,7 @@ from (select string_agg(c, ', ') s from
 	   (select slot_name FROM pg_replication_slots
 	   		             JOIN pg_stat_replication ON pid = active_pid) _(c)) _;
 
-{# Keep this file separate since it might be used by one of our replicas #}
+-- This part show status of the rdflog database itself.
+{# The reason we keep the status report in a separate file is because that file #}
+{# can then be re-used by rdflog replicas #}
 {{ lookup('file', 'status-rdflog.sql') }}
