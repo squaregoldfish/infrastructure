a77ef858e5493f10582bffc3c6cdc07e963d2e3c
André Bjärby <andre.bjarby@nateko.lu.se>
2019-01-21 15:44:56 +0100

    update the stilt/build.sh script to be easier to use without vault
    The svn credentials were extracted from an ansible vault - that part is now
    optional.

--- stilt/build.sh
+++ stilt/build.sh
@@ -2,52 +2,48 @@
 # 1. Build the base stilt image.
 # 2. Build the custom stilt image.
 # 3. Save the custom stilt image as a tarball.
+# 4. Save a log file
 
-set -e
-set -u
-set -x
+set -eux
+set -o pipefail
 
-VAULT=../devops/vault.yml
-VPASS=~/.vault_password 
+TAG_BASE=${TAG_BASE:-stiltbase}
+TAG_CUSTOM=${TAG_CUSTOM:-stiltcustom}
 
-TAG_BASE=stiltbase
-TAG_CUSTOM=stiltcustom
+LOG=$(mktemp /tmp/build-stilt.XXX)
+chmod o-r "$LOG"
 
-read -r -d '' AWK <<'EOF' || :
-$1 == "vault_stilt_jena_user:" { user=$2 }
-$1 == "vault_stilt_jena_password:" { password=$2 }
-END {
-  if (user != "" && password != "") {
-    printf("--build-arg=JENASVNUSER=%s\n", user)
-    printf("--build-arg=JENASVNPASSWORD=%s\n", password)
-  }
+function vault {
+    val=$(cd .. && ansible-vault decrypt --output - vault.yml | 
+                 awk "\$1 ~ /^$1/ { print \$2 }")
+    if [[ -z "$val" ]]; then
+        1>&2 echo "Could not find value for $1"
+        exit 1
+    fi
 }
-EOF
 
-
-readarray -t ARGS < <(ansible-vault decrypt "$VAULT"\
-									--vault-password-file="$VPASS" \
-									--output - | awk "$AWK")
-
-if [[ ${#ARGS[@]} == 0 ]]; then
-	echo "Could not find stilt svn user/password in $VAULT"
-	exit 1
+if [[ "${JENASVNUSER+set}" != set ]]; then
+    JENASVNUSER=$(vault vault_stilt_jena_user)
 fi
 
-BASE_ID=$(docker build -q --tag="$TAG_BASE" "${ARGS[@]}" base)
+if [[ "${JENASVNPASSWORD+set}" != set ]]; then
+    JENASVNPASSWORD=$(vault vault_stilt_jena_password)
+fi
+
+ARGS=(--build-arg=JENASVNUSER=$JENASVNUSER
+      --build-arg=JENASVNPASSWORD=$JENASVNPASSWORD)
+
+docker build --no-cache --pull --tag="$TAG_BASE" "${ARGS[@]}" base | tee "$LOG"
 
 DOCKERFILE="$PWD/custom/Dockerfile.tmp"
 trap 'rm -f -- "$DOCKERFILE"' EXIT
-sed "s/^FROM.*/FROM $BASE_ID/" custom/Dockerfile > "$DOCKERFILE"
+sed "s/^FROM.*/FROM $TAG_BASE/" custom/Dockerfile > "$DOCKERFILE"
 
-# looks like "sha256:0f96e3e9f93f538e6af476ecd0399806ddda44bade7bc0c3f4cd11b49d48e869"
-CUSTOM_ID=$(docker build -q --tag="$TAG_CUSTOM" -f "$DOCKERFILE" custom)
+docker build --no-cache --tag="$TAG_CUSTOM" -f "$DOCKERFILE" custom | tee -a "$LOG"
 
-# looks like "0f96e3e9f93f"
-CSHORT_ID=${CUSTOM_ID:7:12}
-OUTPUT_FN="$TAG_CUSTOM-$CSHORT_ID.tgz"
+CUSTOM_ID=$(docker images | awk "\$1 ~ /^$TAG_CUSTOM\$/ { print \$3 }")
+OUTPUT_FN="$TAG_CUSTOM-$CUSTOM_ID.tgz"
 
 docker save "$TAG_CUSTOM" | gzip -c > "$OUTPUT_FN"
 echo "$OUTPUT_FN"
-
-
+echo "$LOG"
