afeb4e0c6f4996b843637cc62159721ba71132b0
André Bjärby <andre.bjarby@nateko.lu.se>
2019-03-12 10:32:49 +0100

    icos.mailman - override ALLOWED_HOSTS in settings_local

--- devops/roles/icos.mailman/templates/settings_local.py
+++ devops/roles/icos.mailman/templates/settings_local.py
@@ -1,1 +1,10 @@
 # see https://github.com/maxking/docker-mailman/blob/master/web/mailman-web/settings.py for examples of how this file should look
+
+ALLOWED_HOSTS = [
+    "localhost",
+    "mailman-web",
+    "{{ mailman_web_ipv4 }}",
+    {% for host in mailman_domains -%}
+    "{{ host }}",
+    {% endfor %}
+]
