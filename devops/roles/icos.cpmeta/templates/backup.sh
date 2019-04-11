41a21677d135dd701209a0aca679299db430c3e4
André Bjärby <andre.bjarby@nateko.lu.se>
2019-04-11 14:55:21 +0200

    icos.cpmeta - save backup log file and be less spammy with cron

--- devops/roles/icos.cpmeta/templates/backup.sh
+++ devops/roles/icos.cpmeta/templates/backup.sh
@@ -12,4 +12,21 @@ fi
 
 export BORG_UNKNOWN_UNENCRYPTED_REPO_ACCESS_IS_OK=yes
 
-./bin/bbclient-all create --verbose --stats "::$ARCHIVE" "{{ cpmeta_filestorage_target }}"
+LOGFILE=backup.log
+HEADER="=== starting backup"
+
+echo "$HEADER $(date)" >> $LOGFILE
+
+# borg(1) outputs some information to stderr (as well as stdout). This
+# doesn't sit well with cron, which will think that an error occured
+# and send a mail to root.
+# So we capture all output to a logfile, detect when borg fails, and
+# then manually output the logs for the last run.
+if ! ./bin/bbclient-all create --verbose --stats \
+     "::$ARCHIVE" "{{ cpmeta_filestorage_target }}"  \
+     >> "$LOGFILE" 2>&1; then
+    # output everything after the last header
+    sed -ne ":a;\$p;N;/$HEADER/d;ba"  < "$LOGFILE"
+    # signal to cron that we failed
+    exit 1
+fi
