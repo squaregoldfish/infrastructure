79a4312de5097d8f717c9927503dd13200dee212
Jonathan Thiry <jonathan@jonathanthiry.com>
2019-05-07 14:37:11 +0200

    [drupal] Setup borg backup

---
+++ devops/roles/icos.drupal/templates/borgbackup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+set -Eeuo pipefail
+cd {{ drupal_home }}
+
+BB="{{ drupal_home }}/bin/bbclient-all"
+PROJECTS=({{ drupal_websites | join(" ") }})
+LOGFILE=backup.log
+
+for project in "${PROJECTS[@]}"; do
+
+    if [ ! -d "$project" ]; then
+        echo "$project directory not found. Skipping." >> "$LOGFILE"
+        continue
+    fi
+
+    cd $project/drupal
+
+    docker-compose down
+    $BB create --verbose --stats "::$project-{now}" . >> "$LOGFILE"
+    docker-compose up -d
+
+    cd {{ drupal_home }}
+
+done
