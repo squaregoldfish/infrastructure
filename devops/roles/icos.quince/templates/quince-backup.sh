4b0eeafe84484c4703400a7e9245d7340ce41781
André Bjärby <andre.bjarby@nateko.lu.se>
2019-08-23 09:19:44 +0200

    icos.quince - playbook and roles to deploy QuinCe
    https://github.com/BjerknesClimateDataCentre/QuinCe
    https://quince.bcdc.no/

---
+++ devops/roles/icos.quince/templates/quince-backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Backup
+
+set -e
+set -u
+
+tmp=$(mktemp -d /tmp/quince-backup.XXXXXX)
+trap 'rm -rf "$tmp"' EXIT
+cd "$tmp"
+
+# Redirect stderr to real stdout, then stdout to file, then run awk on stderr.
+mysqldump --user=quince --password=quince --hex-blob quince 2>&1 > mysql.dump \
+    | awk '! /Using a password on the command line interface can be insecure./'
+
+bbclient-all create '::{now}' mysql.dump /opt/quince_filestore
+
+bbclient-all prune --keep-within 7d --keep-daily=30 --keep-weekly=150
