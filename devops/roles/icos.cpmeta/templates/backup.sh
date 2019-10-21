e1da0cd450c9aeba2109c70a0bf35a3a305a7ac8
André Bjärby <andre.bjarby@nateko.lu.se>
2019-10-21 10:12:06 +0200

    icos.cpmeta - update backup script
    + The cron job should attach a 'cron' tag to backup archives.
    + Add 'rdfStorage' to things that we backup. This directory is not strictly
      necessary, but recreating it can take a long time.
    + Prune old backups.

--- devops/roles/icos.cpmeta/templates/backup.sh
+++ devops/roles/icos.cpmeta/templates/backup.sh
@@ -17,16 +17,20 @@ HEADER="=== starting backup"
 
 echo "$HEADER $(date)" >> $LOGFILE
 
-# borg(1) outputs some information to stderr (as well as stdout). This
-# doesn't sit well with cron, which will think that an error occured
-# and send a mail to root.
-# So we capture all output to a logfile, detect when borg fails, and
+# borg(1) outputs some information to stderr (as well as stdout). This doesn't
+# sit well with cron, which will think that an error occured and send a mail to
+# root. So we capture all output to a logfile, detect when borg fails, and
 # then manually output the logs for the last run.
-if ! ./bin/bbclient-all create --verbose --stats \
-     "::$ARCHIVE" "{{ cpmeta_filestorage_target }}"  \
+if ! ./bin/bbclient-all create --verbose --stats               \
+     "::$ARCHIVE" "{{ cpmeta_filestorage_target }}" rdfStorage \
      >> "$LOGFILE" 2>&1; then
     # output everything after the last header
     sed -ne ":a;\$p;N;/$HEADER/d;ba"  < "$LOGFILE"
     # signal to cron that we failed
     exit 1
 fi
+
+echo "=== pruning backups" >> $LOGFILE
+./bin/bbclient-all prune --stats                                     \
+                   --keep-within 7d --keep-daily=30 --keep-weekly=50 \
+                   >> $LOGFILE 2>&1
