248aabf193d2385ea1109c0778d3578cbc9d48b3
André Bjärby <andre.bjarby@nateko.lu.se>
2019-11-20 14:41:42 +0100

    restheart - backup using borg

---
+++ devops/roles/icos.restheart/templates/backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -Eueo pipefail
+
+cd "{{ restheart_home }}"
+
+mkdir -p backup
+
+docker-compose exec mongodb mongodump --quiet --archive > backup/server.archive
+
+bin/bbclient-all create '::{now}' backup
+bin/bbclient-all prune --keep-within 7d --keep-daily=30 --keep-weekly=150
+
+rm -rf -- ./backup
