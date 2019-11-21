5a8712df35c1f0221c1000aa3cc1b072be9f9a3e
André Bjärby <andre.bjarby@nateko.lu.se>
2019-11-21 14:07:19 +0100

    restheart - run docker-compose exec with -T as cron doesn't have a terminal

--- devops/roles/icos.restheart/templates/backup.sh
+++ devops/roles/icos.restheart/templates/backup.sh
@@ -6,7 +6,7 @@ cd "{{ restheart_home }}"
 
 mkdir -p backup
 
-docker-compose exec mongodb mongodump --quiet --archive > backup/server.archive
+docker-compose exec -T mongodb mongodump --quiet --archive > backup/server.archive
 
 bin/bbclient-all create '::{now}' backup
 bin/bbclient-all prune --keep-within 7d --keep-daily=30 --keep-weekly=150
