e1489fbd9be20745fdf7999bc5aecfcc6cc1ea17
André Bjärby <andre.bjarby@nateko.lu.se>
2020-10-01 11:00:30 +0200

    jupyter - add documentation to jusers.py

--- devops/roles/icos.jupyter/files/jusers.py
+++ devops/roles/icos.jupyter/files/jusers.py
@@ -1,17 +1,21 @@
 #!/opt/jusers/venv/bin/python3
-import click
 import grp
 import os
 import pwd
+import random
+import spwd
 import subprocess
 import sys
+
+import click
 from ruamel.yaml import YAML
 
 
 PROJECT = '/project'
-DRY_RUN = True
-VERBOSE = True
+DRY_RUN = False
+VERBOSE = False
 USERSDB = '/root/jusers.yml'
+PWDLENG = 10
 
 
 # UTIL
@@ -56,6 +60,14 @@ def list_users():
     return sorted(users)
 
 
+def list_passwordless_users(yaml):
+    for u in yaml['users']:
+        l = u['login']
+        sp = spwd.getspnam(l)
+        if sp.sp_pwdp in ('!', '*'):
+            yield l
+
+
 def is_normal_user(user, uid=None):
     if uid is None:
         try:
@@ -80,11 +92,15 @@ def list_project_groups():
 
 
 def list_user_groups(user):
-    pw_gid = pwd.getpwnam(user).pw_gid
-    return [g.gr_name for g in grp.getgrall()
-            if (g.gr_gid == pw_gid or user in g.gr_mem)
-            and exists(PROJECT, g.gr_name)
-            and g.gr_name != 'common']
+    try:
+        pw_gid = pwd.getpwnam(user).pw_gid
+    except KeyError: # user doesn't exist
+        return []
+    else:
+        return [g.gr_name for g in grp.getgrall()
+                if (g.gr_gid == pw_gid or user in g.gr_mem)
+                and exists(PROJECT, g.gr_name)
+                and g.gr_name != 'common']
 
 
 def diff_user_groups(user):
@@ -97,6 +113,16 @@ def diff_user_groups(user):
     dock = r.stdout.strip()
     host = subprocess.check_output(['id', user], text=1).strip()
     return dock != host
+
+
+def reset_passwords(reset):
+    ps = []
+    for u in reset:
+        p = random.randint(1*(10**PWDLENG), 1*(10**(PWDLENG+1))-1)
+        ps.append((u, p))
+    ls = '\n'.join('%s:%s' % (u, p) for u, p in ps)
+    subprocess.run(['/sbin/chpasswd'], input=ls, check=1, text=1)
+    print("Reset passwords:\n%s" % '\n'.join('  %-15s%s' % (u, p) for u, p in ps))
 
 
 # GENERATE COMMANDS
@@ -141,9 +167,7 @@ def sync_new_users(yaml):
         l = u['login']
         if l in e_users:
             continue
-        yield('useradd --create-home --user-group %s' % l)
-        for g in u.get('groups', []):
-            yield 'usermod -G %s -a %s' % (g, l)
+        yield 'useradd --create-home --user-group %s' % l
 
 
 def sync_user_groups(yaml):
@@ -186,7 +210,7 @@ def run(*gens):
     if DRY_RUN:
         cmds = [l for g in gens for l in g]
         if len(cmds) == 0:
-            return print("Already synced.")
+            return print("Everything is synced.")
         else:
             return print('\n'.join(cmds))
     ncs = 0
@@ -201,7 +225,7 @@ def run(*gens):
             print("abort.")
             sys.exit(1)
     if ncs == 0:
-        print("Already synced.")
+        print("Everything is synced.")
 
 
 # CLI
@@ -210,18 +234,84 @@ def cli():
     pass
 
 
-@cli.command(help="Restart hub (if needed)")
+@cli.command()
 @click.option('--force', '-f', is_flag=True, help="force restart")
 def restart_hub(force):
+    """Restart the hub.
+
+    All users are managed on the host system. The jupyter hub then keeps a copy
+    of the user database. This means that if the user database is updated,
+    then the hub needs to be restarted for those changes to take effect.
+
+    Running this command will restart the hub, but only if the hub doesn't have
+    the latest user database.
+
+    \b
+    $ jusers restart-hub
+    Everything is synced.
+
+
+    \b
+    $ jusers restart-hub --force
+    Restarting hub ...
+
+    """
     run(maybe_restart_hub(force))
 
 
-@cli.command(help='Sync jusers.yaml')
+@cli.command()
 @click.option('--may-the-force-be-with-me', is_flag=True, help="Execute commands.")
 def sync(may_the_force_be_with_me):
+    """Read jusers.yml and output commands needed to sync it.
+
+    In the simplest case, everything is already up-to-date:
+
+    \b
+      $ jusers sync
+      Everything is synced.
+
+    Let's say we add a new user to jusers.yml:
+
+    \b
+       - login: beeblebrox
+         groups:
+           - cake
+           - sausage
+
+    Re-running the sync command will now output the commands needed to move the
+    host closer to what is described by users.yml:
+
+    \b
+      $ jusers sync
+      useradd --create-home --user-group beeblebrox
+      usermod -G cake -a beeblebrox
+      usermod -G sausage -a beeblebrox
+
+    These commands needs to be run manually - either directly or by piping them
+    into bash - until sync reports that 'Everything is synced.'
+
+    The sync command also has the capability to run these commands on its own,
+    in which case it only needs to be run once. This is accomplished by passing
+    the correct option to sync.
+
+    \b
+      $ jusers sync --may-the-force-be-with-me
+      + useradd --create-home --user-group beeblebrox
+      + usermod -G cake -a beeblebrox
+      + usermod -G sausage -a beeblebrox
+      + ln -sf /project/common /home/beeblebrox
+      + mkdir -pm 0755 /home/beeblebrox/project
+      + ln -sf /project/cake /home/beeblebrox/project
+      + ln -sf /project/sausage /home/beeblebrox/project
+      + cd /docker/jupyter
+      + docker-compose restart hub
+      Restarting hub ...
+
+    """
     global DRY_RUN, VERBOSE
-    if may_the_force_be_with_me:
-        DRY_RUN = False
+    if not may_the_force_be_with_me:
+        DRY_RUN = True
+    else:
         VERBOSE = True
     yaml = YAML(typ='safe').load(open(USERSDB, 'rb'))
     run(sync_new_groups(yaml),
@@ -230,6 +320,49 @@ def sync(may_the_force_be_with_me):
         sync_homedirs(yaml),
         maybe_restart_hub(),
         maybe_kick_all_users())
+
+
+@cli.command()
+@click.argument('users', nargs=-1, required=False)
+def set_passwords(users):
+    """Manage user passwords.
+
+    When a user is created, its password is unset. To be able to login, their
+    password needs to be set.
+
+    Running this command without an argument will list all users without
+    passwords.  The command can then be rerun with a list of users whose
+    password is to be reset. The passwords are reset to randomly generated
+    passwords.
+
+    If any passwords were reset, the jupyter hub will be restarted in order for
+    the changes to take affect.
+
+    \b
+      $ jusers set-passwords
+      The following users does not have a password set:
+        alice
+        bob
+        beeblebrox
+    \b
+      $ jusers set-passwords alice bob
+      Reset passwords:
+        alice          48605338769
+        bob            96465289213
+      Restarting hub ...
+
+    """
+    yaml = YAML(typ='safe').load(open(USERSDB, 'rb'))
+    if not users:
+        pwless = list(list_passwordless_users(yaml))
+        if pwless:
+            print("The following users does not have a password set:\n"
+                  "  %s" % '\n  '.join(pwless))
+        else:
+            print("All users have a password set.")
+    else:
+        reset_passwords(users)
+        run(maybe_restart_hub())
 
 
 # MAIN
